81b88de91af972f8a87ea0373c087ba7023255d0
b1tn3r <b1tn3r@gmail.com>
2018-12-07 03:58:37 -0800

    Site Configuration, Disqus Setup

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,15 +3,15 @@ module.exports = {
     siteTitleShort: "GM Starter",                                       // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
     siteTitleAlt: "GatsbyJS Material Starter",                          // Alternative site title for SEO.
     siteLogo: "/logos/logo-1024.png",                                   // Logo used for SEO and manifest.
-    siteUrl: "https://confident-davinci-d948cc.netlify.com",            // Domain of your website without pathPrefix.
+    siteUrl: "confident-davinci-d948cc.netlify.com",            // Domain of your website without pathPrefix.
     pathPrefix: "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
     fixedFooter: false,                                                 // Whether the footer component is fixed, i.e. always visible
     siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
     siteRss: "/rss.xml",                                                // Path to the RSS file.
     siteFBAppID: "1825356251115265",                                    // FB Application ID for using app insights
     siteGATrackingID: "UA-47311644-4",                                  // Tracking code ID for google analytics.
-    // disqusShortname: "gatsby-full-stack-cms",
-    disqusShortname: "https-vagr9k-github-io-gatsby-material-starter",  // Disqus shortname used to load disqus account site.
+    disqusShortname: "gatsby-full-stack-cms",
+    // disqusShortname: "https-vagr9k-github-io-gatsby-material-starter",  // Disqus shortname used to load disqus account site.
     postDefaultCategoryID: "Tech",                                      // Default category for posts.
     dateFromFormat: "YYYY-MM-DD",                                       // Date format used in the frontmatter.
     dateFormat: "DD/MM/YYYY",                                           // Date format for display.
