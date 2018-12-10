0a0edf1b77d6b14789e706f733e6aa36eae80555
b1tn3r <b1tn3r@gmail.com>
2018-12-10 02:09:30 -0800

    Language Translator Added

--- src/components/ToolbarActions/index.jsx
+++ src/components/ToolbarActions/index.jsx
@@ -3,16 +3,38 @@ import UserLinks from "../UserLinks";
 import "./ToolbarActions.scss";
 
 class Toolbar extends Component {
-  render() {
-    const { config } = this.props;
-    return (
-      <div className="toolbar-actions">
-        <div className="userlinks-container">
-          <UserLinks config={config} />
-        </div>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.innerHTML = `
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({
+                    pageLanguage: 'en',
+                    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+                }, 'google_translate_element');
+            }`;
+        script.async = true;
+        document.body.appendChild(script);
+
+        const script2 = document.createElement("script");
+        script2.type = "text/javascript";
+        script2.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script2.async = true;
+        document.body.appendChild(script2);
+    }
+
+    render() {
+        const { config } = this.props;
+        return (
+            <div className="toolbar-actions">
+                <div className="userlinks-container">
+                    <UserLinks config={config} />
+                </div>
+                <div id="google_translate_element" />
+            </div>
+        );
+    }
 }
 
 export default Toolbar;
